1f3005d0149024f15971249e3fe4638759b9d9ad
Sangwon Kim <brave_bean@naver.com>
2026-07-01 17:32:22 +0900

    [refactoring] Match abstaction level and use constexpr

--- SimilarityChecker/LengthChecker.cpp
+++ SimilarityChecker/LengthChecker.cpp
@@ -9,12 +9,20 @@ unsigned int LengthChecker::getSimilarityScore(const string& str1, const string&
 	if (hasSameLength(longer, shorter))
 		return MAX_SCORE;
 
-	double score = (1.0 - (static_cast<double>(getLengthDifference(longer, shorter)) / shorter.length())) * 60.0;
+	if (isGapDoubleThanShorter(longer, shorter))
+		return MIN_SCORE;
+
+	double score = calcScore(longer, shorter);
 
 	if (score < 0)
-		return 0;
+		return MIN_SCORE;
 
 	return static_cast<unsigned int>(score);
+}
+
+double LengthChecker::calcScore(const std::string& longer, const std::string& shorter) const
+{
+	return (1.0 - (static_cast<double>(getLengthDifference(longer, shorter)) / shorter.length())) * static_cast<double>(MAX_SCORE);
 }
 
 const string& LengthChecker::getLonger(const string& str1, const string& str2) const
@@ -29,7 +37,12 @@ const string& LengthChecker::getShorter(const string& str1, const string& str2) 
 
 bool LengthChecker::hasSameLength(const string& longer, const string& shorter) const
 {
-	return longer.length() - shorter.length() == 0;
+	return longer.length() == shorter.length();
+}
+
+bool LengthChecker::isGapDoubleThanShorter(const string& longer, const string& shorter) const
+{
+	return getLengthDifference(longer, shorter) >= (shorter.length() * 2);
 }
 
 unsigned int LengthChecker::getLengthDifference(const string& longer, const string& shorter) const
